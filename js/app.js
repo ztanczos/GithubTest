2dccd956524e728cb47b2009d5ae2bb0449e37fc
Zoltan Tanczos <ztanczos@gmail.com>
2014-01-16 15:07:13 +0100

    added angular routing, my first spa!

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-var githubTestApp = angular.module('githubTestApp', []);
+var githubTestApp = angular.module('githubTestApp', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/helo', {
+                    templateUrl: 'templates/main.html'
+        })
+        .when('/belo', {
+            templateUrl: 'templates/belo.html'
+        })
+        .otherwise({
+           redirectTo: '/helo'
+        });
+    }]);
