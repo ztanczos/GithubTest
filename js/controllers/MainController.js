b0db0e6175b560de6dc6121e318423605074bf82
Zoltan Tanczos <ztanczos@gmail.com>
2014-01-17 11:29:11 +0100

    added input box to add new items

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,8 +2,20 @@
 
 githubTestApp.controller('MainController',
 function MainController($scope, myData) {
+    $scope.data = [];
+
     $scope.btnClick = function() {
         $scope.data = myData.data;
-        console.log('helo');
+    };
+
+    $scope.removeItem = function(item) {
+        var index = $scope.data.indexOf(item);
+        $scope.data.splice(index, 1);
+    };
+
+    $scope.addItem = function(newItem, myForm) {
+        if(myForm.$valid) {
+            $scope.data.push({foo: newItem});
+        }
     };
 });
