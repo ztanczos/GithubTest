8565e5c8b5a25b2dcbe90f689215501c7a618c91
Zoltan Tanczos <ztanczos@gmail.com>
2014-01-17 15:08:09 +0100

    merged gigantic refactoring + added server-side component of SignalR multi-paint

---
+++ MultiPaint.Server/MultiPaint.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MultiPaint.Server.Global" Language="C#" %>
---
+++ MultiPaint.Server/MultiPaint.Server/PaintHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace MultiPaint.Server
+{
+    [HubName("paintHub")]
+    public class PaintHub : Hub
+    {
+        public void DistributeCanvas(string dataUrl)
+        {
+            Clients.All.updateCanvas(dataUrl);
+        }
+    }
+}
